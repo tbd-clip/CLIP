da0d6fc963ac2deb6efff6695d2a02c4407f972b
idigitopia <shrestaa@oregonstate.edu>
2021-02-12 19:29:32 -0800

    refactored visdial_embed.py

--- visdial_embed.py
+++ visdial_embed.py
@@ -9,50 +9,32 @@ import sys
 from PIL import Image
 import numpy as np
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load("ViT-B/32", device=device)
-
-assert len(sys.argv) > 2, "Must specify json path and then path with images"
-
-def create_and_save(json_path, image_root):
+def create_and_save(clip_model, clip_preprocess, json_path, image_root):
 
     embed_size = 512
     print('Reading json...')
     json_data = json.load(open(json_path, 'r'))
 
-    train_embeds = np.zeros((len(json_data['unique_img_train']), embed_size), dtype=np.float32)
+    def get_embeds(json_data):
+        image_embeds = np.zeros((len(json_data), embed_size), dtype=np.float32)
 
-    for idx, path in tqdm(enumerate(json_data['unique_img_train'])):
-        filename = os.path.join(image_root, path)
-        image = preprocess(Image.open(filename)).unsqueeze(0).to(device)
+        for idx, path in tqdm(enumerate(json_data)):
+            filename = os.path.join(image_root, path)
+            image = clip_preprocess(Image.open(filename)).unsqueeze(0).to(device)
 
-        with torch.no_grad(): 
-            image_features = model.encode_image(image)
-            train_embeds[idx] = image_features.cpu()
+            with torch.no_grad():
+                image_features = clip_model.encode_image(image)
+                image_embeds[idx] = image_features.cpu()
 
-    val_embeds = np.zeros((len(json_data['unique_img_val']), embed_size), dtype=np.float32)
+        return image_embeds
 
-    for idx, path in tqdm(enumerate(json_data['unique_img_val'])):
-        filename = os.path.join(image_root, path)
-        image = preprocess(Image.open(filename)).unsqueeze(0).to(device)
-
-        with torch.no_grad(): 
-            image_features = model.encode_image(image)
-            val_embeds[idx] = image_features.cpu()
-
-    test_embeds = np.zeros((len(json_data['unique_img_test']), embed_size), dtype=np.float32)
-
-    for idx, path in tqdm(enumerate(json_data['unique_img_test'])):
-        filename = os.path.join(image_root, path)
-        image = preprocess(Image.open(filename)).unsqueeze(0).to(device)
-
-        with torch.no_grad(): 
-            image_features = model.encode_image(image)
-            test_embeds[idx] = image_features.cpu()
+    train_embeds = get_embeds(json_data['unique_img_train'])
+    val_embeds = get_embeds(json_data['unique_img_val'])
+    test_embeds = get_embeds(json_data['unique_img_test'])
 
     output_h5 = "./data_img.h5"
     print('Saving hdf5 to %s...' % output_h5)
-    f = h5py.File(output_h5, 'w') 
+    f = h5py.File(output_h5, 'w')
     f.create_dataset("images" + '_train', data=train_embeds)
     f.create_dataset("images" + '_val', data=val_embeds)
     f.create_dataset("images" + '_test', data=test_embeds)
@@ -60,5 +42,10 @@ def create_and_save(json_path, image_root):
 
     return 
 
+if __name__ == "__main__":
+    assert len(sys.argv) > 2, "Must specify json path and then path with images"
 
-create_and_save(sys.argv[1], sys.argv[2])
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    clip_model, clip_preprocess = clip.load("ViT-B/32", device=device)
+
+    create_and_save(sys.argv[1], sys.argv[2])
